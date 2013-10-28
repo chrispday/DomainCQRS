e7389380d280cc549f2980e2f859e88a3af79a91
chrispday <chris.p.day@gmail.com>
2013-10-28 19:56:29 +1100

    assembly meta-data

--- DomainCQRS/DomainCQRS.Azure/Properties/AssemblyInfo.cs
+++ DomainCQRS/DomainCQRS.Azure/Properties/AssemblyInfo.cs
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.9.0.0")]
 [assembly: AssemblyFileVersion("0.9.0.0")]
-[assembly: AssemblyDelaySign(true)]
